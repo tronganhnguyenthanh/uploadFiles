967ed7e53a2200bafdd5140353f5b76c5683a40b
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-07-05 17:43:13 +0700

    [UPDATE][FE] upgrade project upload file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
+let file = []
 document.querySelector(".form-control").addEventListener("change", function(e){
   let files = e?.target?.files[0]
   let uploads = URL.createObjectURL(files)
-  document.querySelector("#upload-file").innerHTML += `
-   <details>
-      <summary>${files?.name}</summary>
-      <a href="${uploads}" download="${files?.name}">
-        <p class="ml-5">Download files</p>
-      </a>
-   </details>
-  `
+  file.push(uploads)
+  getFiles(file)
 })
+
+function getFiles(file){
+  let list = "<div class='row p-3'>"
+  file?.forEach(function(item, index){
+    list += `<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+      <div class="card">
+        <img src="${item}" alt="" style="height:400px"/>
+        <button type="button" class="btn btn-danger m-2" onclick="deleteFile(${index})">Delete file</button>
+      </div>
+    </div>
+    `
+  })
+  list += "</div>"
+  document.querySelector("#upload-file").innerHTML = list
+}
+
+function deleteFile(index){
+  let isConfirmed = window.confirm("Are you sure to delete this file?")
+  if(isConfirmed){
+   file.splice(index, 1)
+   getFiles(file)
+  }
+}
